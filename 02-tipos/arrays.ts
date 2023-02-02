88033f8e777816503f43c33a488010e86f7333d9
Angel Travieso <angeltraviesoc@gmail.com>
2023-02-02 17:52:52 -0400

    Arrays"

---
+++ 02-tipos/arrays.ts
@@ -0,0 +1,6 @@
+// nombre:tipo[]
+let arreglo:number[] = [1,2,3,4]; // así ts infiere el tipo
+
+let villano:string[] = ['Omega Rojo', 'Dormamu', 'Duende Verde'];
+
+console.log(villano[0].charAt(0));
---
+++ 02-tipos/dist/arrays.js
@@ -0,0 +1,5 @@
+"use strict";
+// nombre:tipo[]
+let arreglo = [1, 2, 3, 4]; // así ts infiere el tipo
+let villano = ['Omega Rojo', 'Dormamu', 'Duende Verde'];
+console.log(villano[0].charAt(0));
