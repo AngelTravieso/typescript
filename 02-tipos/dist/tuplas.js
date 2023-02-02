fb44e74d46cd64097791cad97e545c2274a6ff25
Angel Travieso <angeltraviesoc@gmail.com>
2023-02-02 17:56:30 -0400

    Tuplas

---
+++ 02-tipos/dist/tuplas.js
@@ -0,0 +1,7 @@
+"use strict";
+// Cuando sabemos el largo del arreglo podemos definir tipos
+let heroes = ['Dr. Strange', 100, true];
+// error
+// heroes[0] = 123;
+// heroes[1] = "123";
+heroes.push(true);
---
+++ 02-tipos/tuplas.ts
@@ -0,0 +1,8 @@
+// Cuando sabemos el largo del arreglo podemos definir tipos
+let heroes:[string, number, boolean] = ['Dr. Strange', 100, true];
+
+// error
+// heroes[0] = 123;
+// heroes[1] = "123";
+
+heroes.push(true);
