29e240905aa582492d04986716e87b3619f03e73
Angel Travieso <angeltraviesoc@gmail.com>
2023-02-02 17:47:50 -0400

    Any

---
+++ 02-tipos/any.ts
@@ -0,0 +1,17 @@
+// any puede ser cualquier cosa
+
+let vengador:any = 'Superman';
+let existe:any;
+let derrotas:any;
+
+vengador = 'Dr. Strange';
+console.log(vengador.charAt(0));
+
+vengador = 150.5555;
+console.log(vengador.toFixed(2));
+
+vengador = true;
+console.log(vengador);
+
+console.log(existe);
+console.log(derrotas);
---
+++ 02-tipos/dist/any.js
@@ -0,0 +1,13 @@
+"use strict";
+// any puede ser cualquier cosa
+let vengador = 'Superman';
+let existe;
+let derrotas;
+vengador = 'Dr. Strange';
+console.log(vengador.charAt(0));
+vengador = 150.5555;
+console.log(vengador.toFixed(2));
+vengador = true;
+console.log(vengador);
+console.log(existe);
+console.log(derrotas);
