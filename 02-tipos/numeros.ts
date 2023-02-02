572d6fb3b12ebf5545d3f1c4c334951813624b68
Angel Travieso <angeltraviesoc@gmail.com>
2023-02-02 17:25:22 -0400

    Numbers

---
+++ 02-tipos/numeros.ts
@@ -0,0 +1,21 @@
+// Formas de declarar numeros
+
+let avengers:number = 5;
+
+// Se debe asignar un valor
+let villanos:number = 0;
+
+let otros = 2;
+
+// villanos = undefined
+if(avengers > villanos) {
+    console.log('Estamos salvados!');
+} else {
+    console.log('Estamos muertos!');
+}
+
+otros = 1234;
+otros =123.423;
+
+// No se puede cambiar el tipo
+// otros = '1234';
