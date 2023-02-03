3e71d92968335b142271766de3269cda76297d99
Angel Travieso <angeltraviesoc@gmail.com>
2023-02-03 11:28:46 -0400

    null y undefined

---
+++ 02-tipos/dist/null_undefined.js
@@ -0,0 +1,7 @@
+"use strict";
+let und = undefined;
+let nul = null;
+let ironman;
+ironman = 'Tony';
+// Error
+// ironman = undefined;
---
+++ 02-tipos/null_undefined.ts
@@ -0,0 +1,9 @@
+let und:undefined = undefined;
+let nul:null = null;
+
+let ironman:string;
+
+ironman = 'Tony';
+
+// Error
+// ironman = undefined;
