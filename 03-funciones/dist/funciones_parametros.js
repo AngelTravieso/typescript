2098229b99d409c2626351e64371033132f32715
AngelTravieso <angeltraviesoc@gmail.com>
2023-02-05 20:43:02 -0400

    parametros opcionales de las funciones

--- 03-funciones/dist/funciones_parametros.js
+++ 03-funciones/dist/funciones_parametros.js
@@ -3,5 +3,16 @@
 function nombreCompleto(nombre, apellido) {
     return `${nombre} ${apellido}`;
 }
+// Parametros opcionales (parametro?tipo)
+function hola(nombre, apellido) {
+    if (apellido) {
+        return `Hola ${nombre} ${apellido}`;
+    }
+    else {
+        return nombre;
+    }
+}
 let nombre = nombreCompleto("clark", "kent");
+let nombre2 = hola("barry");
 console.log(nombre);
+console.log(nombre2);
--- 03-funciones/funciones_parametros.ts
+++ 03-funciones/funciones_parametros.ts
@@ -4,6 +4,18 @@ function nombreCompleto(nombre:string, apellido: string) : string {
     return `${nombre} ${apellido}`;
 }
 
+// Parametros opcionales (parametro?tipo)
+function hola(nombre:string, apellido?:string): string {
+    if(apellido) {
+        return `Hola ${nombre} ${apellido}`;
+    } else {
+        return nombre;
+    }
+}
+
 let nombre = nombreCompleto("clark","kent");
 
+let nombre2 = hola("barry");
+
 console.log(nombre);
+console.log(nombre2);
