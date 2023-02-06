b0a1bcdfd110c79fd2a84e404159b382ec4ea686
AngelTravieso <angeltraviesoc@gmail.com>
2023-02-06 19:45:02 -0400

    metodos dentro de los objetos

--- 04-objetos/dist/objetos_basicos.js
+++ 04-objetos/dist/objetos_basicos.js
@@ -5,10 +5,17 @@ let flash = {
     nombre: "Barry Allen",
     edad: 24,
     poderes: ["Puede correr rápido", "Viajar por el tiemp"],
+    getNombre() {
+        return this.nombre;
+    }
 };
 // Permite reasignar el objeto, pero debe ser con todos sus propiedades
 flash = {
     nombre: "Clark Kent",
     edad: 500,
     poderes: ["Volar"],
+    getNombre() {
+        return this.nombre;
+    },
 };
+flash.getNombre();
--- 04-objetos/objetos_basicos.ts
+++ 04-objetos/objetos_basicos.ts
@@ -1,8 +1,11 @@
 //nombreObjeto:tipo
-let flash: { nombre: string, edad: number, poderes: string[] } = {
+let flash: { nombre: string, edad: number, poderes: string[], getNombre: () => string } = {
     nombre: "Barry Allen",
     edad: 24,
     poderes: ["Puede correr rápido", "Viajar por el tiemp"],
+    getNombre() {
+        return this.nombre;
+    }
 };
 
 // Permite reasignar el objeto, pero debe ser con todos sus propiedades
@@ -10,9 +13,13 @@ flash = {
     nombre: "Clark Kent",
     edad: 500,
     poderes: ["Volar"],
+    getNombre() {
+        return this.nombre;
+    },
 };
 
 
+flash.getNombre();
 
 
 export { };
