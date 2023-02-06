69f0304deac08857de8b7479714d42480e54c2ce
AngelTravieso <angeltraviesoc@gmail.com>
2023-02-06 19:40:05 -0400

    objetos basicos

---
+++ 04-objetos/dist/objetos_basicos.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let flash = {
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Puede correr rápido", "Viajar por el tiemp"],
+};
+// Permite reasignar el objeto, pero debe ser con todos sus propiedades
+flash = {
+    nombre: "Clark Kent",
+    edad: 500,
+    poderes: ["Volar"],
+};
---
+++ 04-objetos/objetos_basicos.ts
@@ -0,0 +1,17 @@
+let flash = {
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Puede correr rápido", "Viajar por el tiemp"],
+};
+
+// Permite reasignar el objeto, pero debe ser con todos sus propiedades
+flash = {
+    nombre: "Clark Kent",
+    edad: 500,
+    poderes: ["Volar"],
+};
+
+
+
+
+export {};
