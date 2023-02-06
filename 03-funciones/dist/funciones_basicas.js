bfc5c10daae7b396801c9baf9ff3d7fe5cd59a4f
AngelTravieso <angeltraviesoc@gmail.com>
2023-02-05 20:33:59 -0400

    funciones basicas

---
+++ 03-funciones/dist/funciones_basicas.js
@@ -0,0 +1,11 @@
+"use strict";
+let heroe = "Flash";
+// function nombre(): retorno
+function imprimeHeroe() {
+    return heroe;
+}
+let activarBatisenal = function () {
+    return "Batiseñal activada.";
+};
+console.log(imprimeHeroe());
+console.log(activarBatisenal());
---
+++ 03-funciones/funciones_basicas.ts
@@ -0,0 +1,13 @@
+let heroe:string = "Flash";
+
+// function nombre(): retorno
+function imprimeHeroe():string {
+    return heroe;
+}
+
+let activarBatisenal = function():string {
+    return "Batiseñal activada.";
+}
+
+console.log(imprimeHeroe());
+console.log(activarBatisenal());
