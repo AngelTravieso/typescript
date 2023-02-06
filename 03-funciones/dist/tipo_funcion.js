be68eba2d99f25a9f7b2db8cf11653704ad06dd6
AngelTravieso <angeltraviesoc@gmail.com>
2023-02-05 21:15:08 -0400

    tipo funcion

---
+++ 03-funciones/dist/tipo_funcion.js
@@ -0,0 +1,22 @@
+"use strict";
+function sumar(a, b) {
+    return a + b;
+}
+function saludar(nombre) {
+    return `I'm ${nombre}`;
+}
+function salvarMundo() {
+    console.log("El mundo está salvado!");
+}
+// reglas para la variable tipo funcion
+// => retorno
+// let miFuncion: (a:number, b:number) => number;
+// let miFuncion: (a:string) => string;
+let miFuncion;
+// miFuncion = 10;
+// miFuncion = sumar;
+// console.log(miFuncion(5, 5));
+// miFuncion = saludar;
+// console.log(miFuncion("Batman"));
+miFuncion = salvarMundo;
+miFuncion();
---
+++ 03-funciones/tipo_funcion.ts
@@ -0,0 +1,29 @@
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+function saludar(nombre: string): string {
+    return `I'm ${nombre}`;
+}
+
+function salvarMundo(): void {
+    console.log("El mundo está salvado!");
+}
+
+// reglas para la variable tipo funcion
+
+// => retorno
+// let miFuncion: (a:number, b:number) => number;
+// let miFuncion: (a:string) => string;
+let miFuncion: () => void;
+
+// miFuncion = 10;
+
+// miFuncion = sumar;
+// console.log(miFuncion(5, 5));
+
+// miFuncion = saludar;
+// console.log(miFuncion("Batman"));
+
+miFuncion = salvarMundo;
+miFuncion();
